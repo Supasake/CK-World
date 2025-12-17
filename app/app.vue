f2001715422e86e616210d40d0fe8f759e9e91a9
Supasake <palasri.sp@gmail.com>
2025-12-17 16:11:29 +0700

    add eslint

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtPage />
+
+    dcddcd
+
+    <div>dvfvfv fvfvfv 1231</div>
+    <h1>1231</h1>
+    n 1111
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>Index</div>
+
+    <div>15151515</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
